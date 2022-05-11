fd81d71df596275da9d749a7db69a76fffee42a7
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-05-11 18:52:21 +0200

    Added fetch - to be updated

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,7 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//  RECUPERATION DU JSON
+async function getPhotographers () {                                  
+    const response = await fetch ("data/photographers.json");  //appel du json
+    const data = await response.json();   // recup des données du json
+    return data;                           //  retourne les données json
+}
+
