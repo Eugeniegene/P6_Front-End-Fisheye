696661ef332c735b949e7c6ac9cfe51d1fe0bc71
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-06-12 19:23:19 +0200

    Linter update

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,45 +1,51 @@
-const photographersSection = document.querySelector(".photograph-header");
-const photographersMedia = document.querySelector(".photograph-media");
+/* eslint-disable linebreak-style */
+/* eslint-disable prefer-const */
+/* eslint-disable no-undef */
+/* eslint-disable eqeqeq */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const photographersSection = document.querySelector('.photograph-header');
+const photographersMedia = document.querySelector('.photograph-media');
 let photographerMedias = [];
 
-//************FETCHING JSON DATA************//
-async function getDataJson () {                                  
-    const response = await fetch ("data/photographers.json");
-    const data = await response.json();
-    return data;
+//* ***********FETCHING JSON DATA************//
+async function getDataJson() {
+  const response = await fetch('data/photographers.json');
+  const data = await response.json();
+  return data;
 }
 
-//************DISPLAY DATA FUNCTION************//
+//* ***********DISPLAY DATA FUNCTION************//
 async function displayData(photographers, medias, iDPhotographer) {
-    photographers.forEach((photographer) => {
-        if(iDPhotographer == photographer.id){
-        const photographerModel = photographerFactory(photographer);
-        const displayPhotographerData = photographerModel.displayPhotographerData();
-        photographerModel.informationsupp();
-        photographersSection.appendChild(displayPhotographerData);
-        displayDataMedia(medias,iDPhotographer,"populaire");
-        }
-    });
-};
+  photographers.forEach((photographer) => {
+    if (iDPhotographer == photographer.id) {
+      const photographerModel = photographerFactory(photographer);
+      const displayPhotographerData = photographerModel.displayPhotographerData();
+      photographerModel.informationsupp();
+      photographersSection.appendChild(displayPhotographerData);
+      displayDataMedia(medias, iDPhotographer, 'populaire');
+    }
+  });
+}
 
-//************FUNCTION GETTING PHOTOGRAPHER MEDIAS AND INFORMATIONS************//
+//* ***********FUNCTION GETTING PHOTOGRAPHER MEDIAS AND INFORMATIONS************//
 async function init(iDPhotographer) {
-    const { photographers } = await getDataJson();
-    const { media } = await getDataJson();
-    
-    displayData(photographers, media, iDPhotographer);
-};
+  const { photographers } = await getDataJson();
+  const { media } = await getDataJson();
 
+  displayData(photographers, media, iDPhotographer);
+}
 
-//************PHOTOGRAPHER URL VISIBLE************//
+//* ***********PHOTOGRAPHER URL VISIBLE************//
 const url = window.location.href;
 const urlArray = url.split('?');
-const iDPhotographerStart = urlArray[urlArray.length-1];
+const iDPhotographerStart = urlArray[urlArray.length - 1];
 
-const menuSelect = document.getElementById("searcher-box");
-menuSelect.addEventListener("change",async function (e) {
-    const {media} = await getDataJson();
-    displayDataMedia(media,iDPhotographerStart, e.target.value);
+//* ***********CHANGE SELECTION************//
+const menuSelect = document.getElementById('searcher-box');
+menuSelect.addEventListener('change', async (e) => {
+  const { media } = await getDataJson();
+  displayDataMedia(media, iDPhotographerStart, e.target.value);
 });
 
 init(iDPhotographerStart);
