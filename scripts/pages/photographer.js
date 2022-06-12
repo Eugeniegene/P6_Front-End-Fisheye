c2c9d72b685ae6c4374032f17e075085424932f5
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-06-12 19:23:37 +0200

    Linter update

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,7 +2,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-undef */
 /* eslint-disable eqeqeq */
-/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const photographersSection = document.querySelector('.photograph-header');
 const photographersMedia = document.querySelector('.photograph-media');
