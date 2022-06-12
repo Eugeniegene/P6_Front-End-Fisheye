42b4c00a6db43d5f2c406d7351f82b988d30c5c7
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-06-12 19:14:12 +0200

    Linter update

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,109 +1,111 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-unused-vars */
 function photographerFactory(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/idPortrait/${portrait}`;
-    const alt = `Portrait de ${name}`;
-    const pageLink = "photographer.html?id=" + id;
+  const picture = `assets/photographers/idPortrait/${portrait}`;
+  const alt = `Portrait de ${name}`;
+  const pageLink = `photographer.html?id=${id}`;
 
-    //************USER CARD CREATED ************//
-    //All data regarding selected photographer will be selected from their id, creating their inital card on the main page//
+  //* ***********USER CARD CREATED ************//
+  //* All data regarding selected photographer will be selected from
+  //* their id, creating their inital card on the main page//
 
-    function getUserCard() {
-        const article = document.createElement( 'article' );
-        const urlPhotographer = document.createElement( 'a' );
-        urlPhotographer.classList.add("photographer-head");
-        urlPhotographer.setAttribute('id', id);
-        urlPhotographer.setAttribute('title', name);
-        urlPhotographer.setAttribute('href', `photographer.html?${id}`);
+  function getUserCard() {
+    const article = document.createElement('article');
 
-        const urlPhotographerImg = document.createElement('div');
-        urlPhotographerImg.classList.add("photographer-head-img");
+    const urlPhotographer = document.createElement('a');
+    urlPhotographer.classList.add('photographer-head');
+    urlPhotographer.setAttribute('id', id);
+    urlPhotographer.setAttribute('title', name);
+    urlPhotographer.setAttribute('href', `photographer.html?${id}`);
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute('role', 'img');
-        img.setAttribute('alt', alt);
+    const urlPhotographerImg = document.createElement('div');
+    urlPhotographerImg.classList.add('photographer-head-img');
 
-        const nom = document.createElement( 'h2' );
-        nom.textContent = name;
+    const img = document.createElement('img');
+    img.setAttribute('src', picture);
+    img.setAttribute('role', 'img');
+    img.setAttribute('alt', alt);
 
-        const location = document.createElement( 'h3' );
-        location.textContent = city + ", " + country;
+    const nom = document.createElement('h2');
+    nom.textContent = name;
 
-        const citation = document.createElement( 'p' );
-        citation.textContent = tagline;
-        
-        const prix = document.createElement( 'p' );
-        prix.textContent = price + "€/jour";
-        prix.className = 'price';
+    const location = document.createElement('h3');
+    location.textContent = `${city}, ${country}`;
 
-        const photographerTags = document.createElement( 'ul' );
-        photographerTags.classList.add("photographerTags");
+    const citation = document.createElement('p');
+    citation.textContent = tagline;
 
-        //APPEND SECTION
-        article.appendChild(urlPhotographer);
-        urlPhotographer.appendChild(img);
-        urlPhotographer.appendChild(nom);
-        urlPhotographer.appendChild(location);
-        urlPhotographer.appendChild(citation);
-        urlPhotographer.appendChild(prix);
+    const prix = document.createElement('p');
+    prix.textContent = `${price}€/jour`;
+    prix.className = 'price';
 
-        return (article);
-    }
-    
-    //************ALL PHOTOGRAPHER ON PHOTOGRAPHER PAGE************//
-    //The following information will be available by clicking on a card that will display all photographer data//
-    function displayPhotographerData() {
+    //* APPEND SECTION
+    article.appendChild(urlPhotographer);
+    urlPhotographer.appendChild(img);
+    urlPhotographer.appendChild(nom);
+    urlPhotographer.appendChild(location);
+    urlPhotographer.appendChild(citation);
+    urlPhotographer.appendChild(prix);
 
-        const photographerData = document.createElement('div');
+    return (article);
+  }
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute('role', 'img');
-        img.setAttribute('alt', alt);
-        img.setAttribute('id','portraitPicture')
+  //* ***********ALL PHOTOGRAPHER ON PHOTOGRAPHER PAGE************//
+  //* The following information will be available by clicking on a card
+  //* that will display all photographer data//
+  function displayPhotographerData() {
+    const photographerData = document.createElement('div');
 
-        const nom = document.createElement( 'h2' );
-        nom.textContent = name;
-        nom.setAttribute("name", nom)
+    const img = document.createElement('img');
+    img.setAttribute('src', picture);
+    img.setAttribute('role', 'img');
+    img.setAttribute('alt', alt);
+    img.setAttribute('id', 'portraitPicture');
 
-        const location = document.createElement( 'h3' );
-        location.textContent = city + ", " + country;
-        location.setAttribute ("location", location)
+    const nom = document.createElement('h2');
+    nom.textContent = name;
+    nom.setAttribute('name', nom);
 
-        const citation = document.createElement( 'p' );
-        citation.textContent = tagline;
-        citation.className="tagline";
+    const location = document.createElement('h3');
+    location.textContent = `${city}, ${country}`;
+    location.setAttribute('location', location);
 
-        photographerData.appendChild(img);
-        photographerData.appendChild(nom);
-        photographerData.appendChild(location);
-        photographerData.appendChild(citation);
+    const citation = document.createElement('p');
+    citation.textContent = tagline;
+    citation.className = 'tagline';
 
-        document.getElementById("contact_modal-photographer-name").innerHTML = 
-        document.getElementById("contact_modal-photographer-name").innerHTML + "<br/>" + data.name; 
+    photographerData.appendChild(img);
+    photographerData.appendChild(nom);
+    photographerData.appendChild(location);
+    photographerData.appendChild(citation);
 
-        return photographerData;
-    }
+    document.getElementById('contact_modal-photographer-name').innerHTML =
+    `${document.getElementById('contact_modal-photographer-name').innerHTML}<br/>${data.name}`;
 
-    //************BOTTOM BANNER WITH ALL LIKES AND PRICE/DAY************//
-    function informationsupp(){
-    
-        const informationSuppl = document.querySelector(".informationsupp");
-        const like = document.createElement("h4")
-        const emoji = document.createElement("i");
-        const prix = document.createElement("h4");
-    
-        like.id = "all-likes";
-        emoji.className = "fas fa-heart fa-lg";
-        prix.textContent = price + "€/jour";
-        prix.id = "prix";
-        
-        informationSuppl.appendChild(like);
-        informationSuppl.appendChild(emoji);
-        informationSuppl.appendChild(prix);
-        
-    }
+    return photographerData;
+  }
 
-    return { name, picture, getUserCard, displayPhotographerData, informationsupp }
+  //* ***********BOTTOM BANNER WITH ALL LIKES AND PRICE/DAY************//
+  function informationsupp() {
+    const informationSuppl = document.querySelector('.informationsupp');
+    const like = document.createElement('h4');
+    const emoji = document.createElement('i');
+    const prix = document.createElement('h4');
+
+    like.id = 'all-likes';
+    emoji.className = 'fas fa-heart fa-lg';
+    prix.textContent = `${price}€/jour`;
+    prix.id = 'prix';
+
+    informationSuppl.appendChild(like);
+    informationSuppl.appendChild(emoji);
+    informationSuppl.appendChild(prix);
+  }
+
+  return {
+    name, picture, getUserCard, displayPhotographerData, informationsupp,
+  };
 }
