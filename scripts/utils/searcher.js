5a3d43c88197f8ee660dc568dba4abff8769a257
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-06-16 15:57:14 +0200

    Final searcher update

--- scripts/utils/searcher.js
+++ scripts/utils/searcher.js
@@ -1,34 +1,57 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable func-names */
-/* eslint-disable no-caller */
-/* eslint-disable default-case */
-/* eslint-disable no-use-before-define */
-/* eslint-disable prefer-const */
-/* eslint-disable no-unused-vars */
-/* eslint-disable eqeqeq */
-/* eslint-disable no-plusplus */
-/* eslint-disable camelcase */
-/* eslint-disable no-undef */
+let photographerMedias = [];
+
 function displayPhotographerMedia(medias, photographerId) {
+  const photographersMedia = document.querySelector('.photograph-media');
   photographersMedia.innerHTML = ' ';
-  let all_likes = 0;
+  let allLikes = 0;
   let position = 0;
   photographerMedias = [];
-  const all_likesElement = document.getElementById('all-likes');
+  const allLikesElement = document.getElementById('all-likes');
   medias.forEach((media) => {
-    if (media.photographerId == photographerId) {
+    if (media.photographerId === Number(photographerId)) {
+      // mediaListFactory is defined in media.js
+      // eslint-disable-next-line no-undef
       const mediaModel = mediaListFactory(media);
       const userMediaDOM = mediaModel.mediasCardDOM(position);
       photographersMedia.appendChild(userMediaDOM);
-      all_likes += media.likes;
+      allLikes += media.likes;
       photographerMedias.push(mediaModel);
-      position++;
+      position += 1;
     }
   });
-  all_likesElement.innerHTML = all_likes;
+  allLikesElement.innerHTML = allLikes;
 }
 
 //* ***********MEDIA SORTER FUNCTION BY NAME************//
+
+function heartElement(e) {
+  if (!e.classList.contains('isClicked')) {
+    e.classList.add('isClicked');
+    const nbreLike = e.parentElement.children[1];
+    const textNumberMedia = Number(nbreLike.textContent) + 1;
+    nbreLike.textContent = textNumberMedia;
+
+    const allLikes = document.getElementById('all-likes');
+    const textNumberBanner = Number(allLikes.textContent) + 1;
+    allLikes.textContent = textNumberBanner;
+  }
+}
+//* ***********LIKE FUNCTION************//
+function likeClick() {
+  const hearts = document.querySelectorAll('.heart');
+  hearts.forEach((e) => {
+    e.addEventListener('click', () => {
+      heartElement(e);
+    });
+    e.addEventListener('keydown', (event) => {
+      if (event.keyCode === 13) {
+        heartElement(e);
+      }
+    });
+  });
+}
+// used in another file
+// eslint-disable-next-line no-unused-vars
 function displayDataMedia(medias, photographerId, value) {
   switch (value) {
     case 'populaire':
@@ -40,31 +63,10 @@ function displayDataMedia(medias, photographerId, value) {
     case 'titre':
       medias.sort((a, b) => a.title.localeCompare(b.title));
       break;
+    default:
+      medias.sort((a, b) => b.likes - a.likes);
+      break;
   }
   displayPhotographerMedia(medias, photographerId);
   likeClick();
-}
-//* ***********LIKE FUNCTION************//
-function likeClick() {
-  const hearts = document.querySelectorAll('.heart');
-  hearts.forEach((e) => {
-    e.addEventListener('click', function () {
-      const nbreLike = e.parentElement.children[1];
-      nbreLike.textContent++;
-      let all_likes = document.getElementById('all-likes');
-      all_likes.innerHTML++;
-      // eslint-disable-next-line no-restricted-properties
-      e.removeEventListener('click', arguments.callee);
-    });
-    e.addEventListener('keydown', function (event) {
-      if (event.keyCode == 13) {
-        const nbreLike = e.parentElement.children[1];
-        nbreLike.textContent++;
-        let all_likes = document.getElementById('all-likes');
-        all_likes.innerHTML++;
-        // eslint-disable-next-line no-restricted-properties
-        e.removeEventListener('keydown', arguments.callee);
-      }
-    });
-  });
 }
