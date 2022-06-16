a0de99f06fc99e55f0283f260ba842da02616484
Eugénie <97299297+Eugeniegene@users.noreply.github.com>
2022-06-16 18:21:05 +0200

    Final Lightbox update

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,11 +1,5 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable camelcase */
-/* eslint-disable no-plusplus */
-/* eslint-disable no-param-reassign */
+// photographerMedias is called numerous times in this file, but is only defined in searcher.js
 /* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
-const media = document.getElementsByClassName('media_display_lightbox');
 const lightbox = document.getElementById('lightbox');
 const rightArrow = document.getElementById('next');
 const leftArrow = document.getElementById('previous');
@@ -15,34 +9,50 @@ function lightboxMediaOpen() {
   return lightbox;
 }
 
-//* ***********Close lightbox section************//
-const closeBtn = document.querySelector('.lightbox_close');
-document.addEventListener('keydown', keydown, false);
-
+//* ***********Close lightbox function************//
 function closeLightbox() {
   lightbox.style.display = 'none';
 }
 
-// eslint-disable-next-line func-names
-closeBtn.onclick = function () {
-  closeLightbox();
-};
-
+//* ***********Lightbox display media and images function************//
+function displayVideoImage(count) {
+  lightboxMediaOpen();
+  if (photographerMedias.length > count && count >= 0) {
+    if (photographerMedias[count].image) {
+      const img = document.getElementById('lightbox_image_photographer');
+      img.style.display = 'block';
+      img.setAttribute('tabindex', '4');
+      img.setAttribute('src', `assets/photographers/${photographerMedias[count].photographerId}/${photographerMedias[count].image}`);
+      const videoLightbox = document.getElementById('lightbox_video_photographer');
+      videoLightbox.style.display = 'none';
+    } else if (photographerMedias[count].video) {
+      const video = document.getElementById('lightbox_video_photographer');
+      video.style.display = 'block';
+      video.setAttribute('tabindex', '4');
+      video.setAttribute('src', `assets/photographers/${photographerMedias[count].photographerId}/${photographerMedias[count].video}`);
+      const imageLightbox = document.getElementById('lightbox_image_photographer');
+      imageLightbox.style.display = 'none';
+    }
+    const pictureTitle = document.getElementById('lightbox_title');
+    pictureTitle.textContent = photographerMedias[count].title;
+    lightbox.setAttribute('position', count);
+  }
+}
 //* ***********Previous and next button created************//
 //* count relates to selected media's position. Every media has a number.
 //* This number allows lightbox controls to switch from one to another
 //* regarding selected media's number//
+
 const previousMedia = (count) => {
-  count -= 1;
-  displayVideoImage(count);
+  const countTemp = Number(count) - 1;
+  displayVideoImage(countTemp);
 };
 const nextMedia = (count) => {
-  count++;
-  displayVideoImage(count);
+  const countTemp = Number(count) + 1;
+  displayVideoImage(countTemp);
 };
+
 //* ***********Right button lightbox section************//
-rightArrow.addEventListener('click', nextBtn, false);
-leftArrow.addEventListener('click', previousBtn, false);
 async function previousBtn() {
   const positionMedia = lightbox.getAttribute('position');
   previousMedia(positionMedia);
@@ -51,29 +61,10 @@ async function nextBtn() {
   const positionMedia = lightbox.getAttribute('position');
   nextMedia(positionMedia);
 }
-//* ***********Lightbox display media and images function************//
-function displayVideoImage(count) {
-  if (photographerMedias.length > count && count >= 0) {
-    if (photographerMedias[count].image) {
-      const img = document.getElementById('lightbox_image_photographer');
-      img.style.display = 'block';
-      img.setAttribute('tabindex', '4');
-      img.setAttribute('src', `assets/photographers/${photographerMedias[count].photographerId}/${photographerMedias[count].image}`);
-      const video_lightbox = document.getElementById('lightbox_video_photographer');
-      video_lightbox.style.display = 'none';
-    } else if (photographerMedias[count].video) {
-      const video = document.getElementById('lightbox_video_photographer');
-      video.style.display = 'block';
-      video.setAttribute('tabindex', '4');
-      video.setAttribute('src', `assets/photographers/${photographerMedias[count].photographerId}/${photographerMedias[count].video}`);
-      const image_lightbox = document.getElementById('lightbox_image_photographer');
-      image_lightbox.style.display = 'none';
-    }
-    const pictureTitle = document.getElementById('lightbox_title');
-    pictureTitle.textContent = photographerMedias[count].title;
-    lightbox.setAttribute('position', count);
-  }
-}
+rightArrow.addEventListener('click', nextBtn, false);
+leftArrow.addEventListener('click', previousBtn, false);
+
+//* ***********Keydown functions ************//
 function keydown(e) {
   if (e.keyCode === 27) {
     closeLightbox();
@@ -85,3 +76,12 @@ function keydown(e) {
     nextBtn();
   }
 }
+
+//* ***********Close lightbox event listener************//
+const closeBtn = document.querySelector('.lightbox_close');
+document.addEventListener('keydown', keydown, false);
+
+// eslint-disable-next-line func-names
+closeBtn.onclick = function () {
+  closeLightbox();
+};
